b602905b15ae4296db1a0e46c31d14f19a964efe
igorbdaluz <igorbdaluz@hotmail.com>
2021-04-21 16:08:31 -0300

    Paginacao 2 commit

--- LojaVirtual.Web/Controllers/VitrineController.cs
+++ LojaVirtual.Web/Controllers/VitrineController.cs
@@ -1,4 +1,5 @@
 ﻿using LojaVirtual.Dominio.Entidades;
+using LojaVirtual.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +11,25 @@ namespace LojaVirtual.Web.Controllers
     public class VitrineController : Controller
     {
         private LojaDbContext context;
-        public int produtosPorPagina = 3;
+        public int produtosPorPagina = 2;
 
         // GET: Vitrine
-        public ActionResult ListaProdutos(int pagina = 1)
+        public ViewResult ListaProdutos(int pagina = 1)
         {
             context = new LojaDbContext();
 
-            var produtos = context.Produtos.OrderBy(p => p.Descricao).Skip((pagina - 1) * produtosPorPagina).Take(produtosPorPagina);
-
-            return View(produtos);
+            ProdutoViewModel produtoViewModel = new ProdutoViewModel()
+            {
+                Produtos = context.Produtos.OrderBy(p => p.Descricao).Skip((pagina - 1) * produtosPorPagina).Take(produtosPorPagina),
+                Paginacao = new Paginacao()
+                {
+                    PaginaAtual = pagina,
+                    ItensPorPagina = produtosPorPagina,
+                    ItensTotal = context.Produtos.Count()
+                }
+            };
+                 
+            return View(produtoViewModel);
         }
     }
 }
---
+++ LojaVirtual.Web/Models/ProdutoViewModel.cs
@@ -0,0 +1,11 @@
+﻿using LojaVirtual.Dominio.Entidades;
+using System.Collections.Generic;
+
+namespace LojaVirtual.Web.Models
+{
+    public class ProdutoViewModel
+    {
+        public IEnumerable<Produto> Produtos;
+        public Paginacao Paginacao { get; set; }
+    }
+}
