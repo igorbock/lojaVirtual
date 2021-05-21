991f5f00b6621c3bc1b95f80815246d2c9322a50
igorbdaluz <igorbdaluz@hotmail.com>
2021-05-20 21:27:05 -0300

    Carrinho ItensCarrinho

---
+++ LojaVirtual.Dominio/Entidades/Carrinho.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Dominio.Entidades
+{
+    public class Carrinho
+    {
+        private readonly List<ItemCarrinho> ProdutosCarrinho = new List<ItemCarrinho>();
+
+        //Adicionar
+        public void AdicionarItemCarrinho(Produto produto, int quantidade)
+        {
+            ItemCarrinho item = ProdutosCarrinho.FirstOrDefault(p => p.Produto.ProdutoId == produto.ProdutoId);
+
+            if(item == null)
+            {
+                ProdutosCarrinho.Add(new ItemCarrinho { Produto = produto, Quantidade = quantidade });
+            }
+            else
+            {
+                item.Quantidade += quantidade;
+            }
+        }
+
+        //Remover
+        public void RemoverItem(Produto produto)
+        {
+            ProdutosCarrinho.RemoveAll(p => p.Produto.ProdutoId == produto.ProdutoId);
+        }
+
+        //Obter valor total
+        public decimal ObterValorTotal() 
+        {
+            return ProdutosCarrinho.Sum(p => p.Produto.Preco * p.Quantidade);
+        }
+
+        //Limpar carrinho
+        public void LimparCarrinho()
+        {
+            ProdutosCarrinho.Clear();
+        }
+
+        //Itens carrinho
+        public IEnumerable<ItemCarrinho> ItensCarrinho { get { return ProdutosCarrinho; } }
+    }
+}
